d8f0c9fed12f917c6f0ec834747888f3a382618f
Pales0tine <mmii1662013@gmail.com>
2026-07-18 09:04:32 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-
+const bedrock = require('bedrock-protocol');
+const http = require('http');
+http.createServer((req, res) => {
+  res.write("Bot is running!");
+  res.end();
+}).listen(8080);
+function startBot() {
+  const bot = bedrock.createClient({
+    host: '5ld_501.aternos.me',
+    port: 11529,
+    username: 'AFK_Bot_Mobile',
+    offline: true,
+    version: '1.26.0.2'
+  });
+  bot.on('spawn', () => console.log('البوت دخل!'));
+  bot.on('close', () => setTimeout(startBot, 5000));
+  bot.on('error', (err) => console.log(err.message));
+}
+startBot();
